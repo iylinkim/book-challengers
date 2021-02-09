9739b05b5f2a5e58f57811db8d17798fa9600ff0
iylinkim <kiylin94@naver.com>
2021-02-09 23:44:26 +0900

    build UI Auth component

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,8 +1,9 @@
 import { authService, firebaseInstance } from "fbase";
 import React, { useState } from "react";
+import styles from "components/auth.module.css";
 
 const Auth = () => {
-  console.log('Auth');
+  console.log("Auth");
   const [newAccount, setNewAccount] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -52,39 +53,80 @@ const Auth = () => {
     }
   };
   return (
-    <>
-      <form onSubmit={onSubmit}>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={onChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={password}
-          onChange={onChange}
-          required
-        />
-        <input
-          type="submit"
-          value={newAccount ? "Create Account" : "Sign In"}
-        />
-      </form>
-      <p onClick={toggleLoginSignout}>
-        {newAccount ? "Sign In" : "Create Account"}
-      </p>
-      <button onClick={onSocialSignIn} name="Google">
-        Sign In with Google
-      </button>
-      <button onClick={onSocialSignIn} name="Github">
-        Sign In with Github
-      </button>
-    </>
+    <div className={styles.wrap}>
+      <div className={styles.img}>
+        <img src="authMain.jpg" alt="auth main" />
+      </div>
+      <div className={styles.info_wrap}>
+        <div className={styles.info}>
+          <form onSubmit={onSubmit} className={styles.form}>
+            <div className={styles.email}>
+              <span className={`${styles.box} ${styles.icon}`}>
+                <i className="fas fa-envelope"></i>
+              </span>
+              <input
+                className={`${styles.box} ${styles.input}`}
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={email}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className={styles.password}>
+              <span className={`${styles.box} ${styles.icon}`}>
+                <i className="fas fa-lock"></i>
+              </span>
+              <input
+                className={`${styles.box} ${styles.input}`}
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <input
+              className={`${styles.box} ${styles.submit}`}
+              type="submit"
+              value={newAccount ? "Create Account" : "Sign In"}
+            />
+          </form>
+
+          <p onClick={toggleLoginSignout} className={styles.toggle}>
+            {newAccount
+              ? "Already have an account?"
+              : "dont't have an account?"}
+            <span>{newAccount ? "Sign In" : "Create Account"}</span>
+          </p>
+          <span className={styles.social_text}>Or, Sign In with</span>
+          <p className={styles.social_wrap}>
+            <button
+              onClick={onSocialSignIn}
+              name="Google"
+              className={`${styles.box} ${styles.social}`}
+            >
+              <span>
+                <i className="fab fa-google"></i>
+              </span>
+              Google
+            </button>
+            <button
+              onClick={onSocialSignIn}
+              name="Github"
+              className={`${styles.box} ${styles.social}`}
+            >
+              <span>
+                <i className="fab fa-github"></i>
+              </span>
+              Github
+            </button>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
